769b10c693fbdacf3438d4e6aa6efd2d564b10a6
Gabriel Matos <gabrielmouromatos@gmail.com>
2020-12-26 18:04:52 -0300

    Criadno operaco de instanciacao

--- src/main/java/com/gabrielmatos/cursomc/CursomcApplication.java
+++ src/main/java/com/gabrielmatos/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.gabrielmatos.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.gabrielmatos.cursomc.domain.Categoria;
+import com.gabrielmatos.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria categoria1 = new Categoria(null, "Infomática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2));
+		
+		
 	}
 
 }
