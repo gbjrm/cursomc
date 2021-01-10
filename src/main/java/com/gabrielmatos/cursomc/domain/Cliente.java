1b92092e01c72290949f67ceb69d0ea7bca26aef
Gabriel Matos <gabrielmouromatos@gmail.com>
2021-01-10 19:00:44 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/gabrielmatos/cursomc/domain/Cliente.java
+++ src/main/java/com/gabrielmatos/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -30,7 +31,8 @@ public class Cliente implements Serializable {
 	private Integer tipoCliente;
 	
 	
-	@OneToMany(mappedBy = "cliente")
+	//Se um cliente for apagado, os endereços dele também serão.
+	@OneToMany(mappedBy = "cliente", cascade=CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
